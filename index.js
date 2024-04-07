9b09cfc8b0e7340fe255208702433ad5c5faf211
ezekielswanson <swansonezekiel@gmail.com>
2024-04-06 21:40:18 -0500

    updated express static middleware function

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const express = require('express');
 //storing instance of express app
 const app = express()
 
+app.use(express.static('views'))
+
+
+
 
 //listen for request 
-
 app.listen(3000)
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 
     //automatically sets content type header
     res.send('<p>Home page </p>')
 })
 
-app.get('/', (req,res) => {
+
+
+app.get('/about', (req, res) => {
 
     //automatically sets content type header
-    res.send('<p>Home page </p>')
-})
-
-app.get('/about', (req,res) => {
-
-    //automatically sets content type header
-    res.send('<p>About Page</p>')
+    res.sendFile('<p>About Page</p>')
 })
