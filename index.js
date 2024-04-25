4cb9b6471a10c5c6ae40b482960f97b69d575076
ezekielswanson <swansonezekiel@gmail.com>
2024-04-24 21:32:02 -0500

    Updated index file

--- index.js
+++ index.js
@@ -1,87 +1,53 @@
-
 const express = require('express');
 const morgan = require('morgan');
 
-//storing instance of express app
-const app = express()
+// Storing instance of express app
+const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
 
-//Register view engine
-app.set('view engine', 'ejs')
+// Morgan logging middleware
+app.use(morgan('dev'));
 
-
-app.use((req, res) => {
+// General request logging middleware - enhanced with next to continue the cycle
+app.use((req, res, next) => {
     console.log('new request made:');
     console.log('host: ', req.hostname);
     console.log('path: ', req.path);
     console.log('method: ', req.method);
+    next(); // Continue to the next middleware or route handler
+});
 
-})
+// Routes
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs: blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
+
+// 404 page - moved to be the last middleware
+app.use((req, res) => {
+    res.status(404).render('404');
+});
 
 // Listen for requests and add error handling
 const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
-
-// Error handling
-server.on('error', (error) => {
-  console.error('Server error:', error);
-});
-
-
-app.use(morgan('dev'));
-
-
-//Take a view - render it, sennd back to broswer
-//Automatically sets content type header
-app.get('/', (req, res) => {
-    const blogs = [
-      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-  
-    res.render('index', { title: 'Home', blogs: blogs,  });
-  });
-
-
- 
-  
-
-
-
-app.get('/about', (req, res) => {
-
-    /* automatically sets content type header
-    sends a html string response back to client
-    res.send('<p>About Page</p>')
-    */
-
-   res.render('about', {title: 'About'})
-
-
-    
-})
-
-
-
 
 // Error handling
 server.on('error', (error) => {
     console.error('Server error:', error);
 });
-
-
-//inside render method -> send back create "view"
-app.get('/blogs/create', (req, res) => {
-    res.render('create')
-})
-
-
-/* 404 page
--Works as a catch all, if nothing else matches send the user to the 404 page.
-
-*/
-app.use((req, res) => {
-    res.status(404).render('404');
-  });
