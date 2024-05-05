6d14f662e04b240a04e02f158ab2e2407ac163de
ezekielswanson <swansonezekiel@gmail.com>
2024-05-04 21:10:12 -0500

    Fixed server error

--- index.js
+++ index.js
@@ -5,44 +5,35 @@ const mongoose = require('mongoose');
 // Storing instance of express app
 const app = express();
 
-// Register view 
+// Register view engine
 app.set('view engine', 'ejs');
 
 // Morgan logging middleware
 app.use(morgan('dev'));
 
-
-//connect to mongoDB
+// Connect to MongoDB
 const dbURI  = 'mongodb+srv://gbaby16:gbaby16@cluster0.urkpacq.mongodb.net/?retryWrites=true&w=majority';
 
 /*
--This is an async line of code so it returns a promise
--add then method on the promsise
--then method takes a call back function
+- This is an async line of code so it returns a promise
+- add then method on the promise
+- then method takes a callback function
 */
-
-
-//Connect to MongoDB
-//Once the promise resolves then...
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000)
+.then((result) => {
+    const server = app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+
+    // Error handling for server
+    server.on('error', (error) => {
+        console.error('Server error:', error);
+    });
+})
 .catch((err) => console.log('Check your connection with the DB', err));
 
-
-// Listen for requests and add error handling
-//app.listen(3000)
-
-
-/*
-const server = app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-*/
-
-
-//Rendering static files
+// Rendering static files
 app.use(express.static('public'));
-
 
 // General request logging middleware - enhanced with next to continue the cycle
 app.use((req, res, next) => {
@@ -75,11 +66,3 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404');
 });
-
-
-// Error handling
-server.on('error', (error) => {
-    console.error('Server error:', error);
-});
-
-
