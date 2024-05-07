d05a21c35940b57db61d0a70264072dcc14d9c71
ezekielswanson <swansonezekiel@gmail.com>
2024-05-06 21:31:40 -0500

    Updated blog and index.js files

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 
-
-
 // Storing instance of express app
 const app = express();
 
@@ -14,8 +12,7 @@ app.set('view engine', 'ejs');
 // Morgan logging middleware
 app.use(morgan('dev'));
 
-
-//Mongoose and Mongo Sandbox routes 
+// Mongoose and Mongo Sandbox routes
 app.get('/add-blog', (req, res) => {
     const blog = new Blog({
         title: 'new blog',
@@ -23,6 +20,7 @@ app.get('/add-blog', (req, res) => {
         body: 'click here to read more!'
     });
 
+    // Saves data to the db
     blog.save()
         .then((result) => {
             res.send(result); // Added semicolon
@@ -33,14 +31,35 @@ app.get('/add-blog', (req, res) => {
         }); // Closed the catch block properly
 });
 
+// Retrieve all blogs from collection
+app.get('/all-blogs', (req, res) => {
+    // Finding all documents in the blog collection
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('An error occurred while retrieving the blogs'); // Corrected error response
+        }); // Closed the catch block properly
+});
 
 
-
-
-
+app.get('/single-blog', (req, res) => {
+    Blog.findById('id-goes-here')
+      // Then take result from DB
+      .then((result) => {
+        // Send as response to browser
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  
 
 // Connect to MongoDB
-const dbURI  = 'mongodb+srv://gbaby16:gbaby16@cluster0.urkpacq.mongodb.net/?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://gbaby16:gbaby16@cluster0.urkpacq.mongodb.net/?retryWrites=true&w=majority';
 
 /*
 - This is an async line of code so it returns a promise
@@ -48,22 +67,22 @@ const dbURI  = 'mongodb+srv://gbaby16:gbaby16@cluster0.urkpacq.mongodb.net/?retr
 - then method takes a callback function
 */
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => {
-    const server = app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
+    .then((result) => {
+        const server = app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
 
-    // Error handling for server
-    server.on('error', (error) => {
-        console.error('Server error:', error);
-    });
-})
-.catch((err) => console.log('Check your connection with the DB', err));
+        // Error handling for server
+        server.on('error', (error) => {
+            console.error('Server error:', error);
+        });
+    })
+    .catch((err) => console.log('Check your connection with the DB', err));
 
 // Rendering static files
 app.use(express.static('public'));
 
-// General request logging middleware - enhanced with next to continue the cycle
+// General request logging middleware - enhanced with `next` to continue the cycle
 app.use((req, res, next) => {
     console.log('new request made:');
     console.log('host: ', req.hostname);
@@ -75,15 +94,15 @@ app.use((req, res, next) => {
 // Routes
 app.get('/', (req, res) => {
     const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
     ];
     res.render('index', { title: 'Home', blogs: blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {title: 'About'});
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -94,13 +113,3 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404');
 });
-
-
-
-
-
-
-
-
-
-
--- models/blog.js
+++ models/blog.js
@@ -1,63 +1,20 @@
-//require the library (also known as libraries or packages)
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-/*
--Schema defines the structure of our doc's
--schema is what the model wraps aroud
-*/
-
-const Schema = mongoose.Schmea;
-
-//Constuctor function that creats new instace of an object - and pass in obj as parameter
-/*
-   title with a nested object of properties & values 
-   type = string 
-   and the type has to be a string *denoted by the required property.
-*/
 const blogSchema = new Schema({
-    title:  {
-        type: string,
-        required: true
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  snippet: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true });
 
-    snippet: {
-        type: string,
-        required: true
-    },
-    body: {
-        type: string,
-        required: true
-    }
-
-/*Time stamps create time stamped properties on our blog doc such as 
--created at
--updated at 
-
--Everytime we create blog doc in the future 
-these additional time stamp options auto assign our schmea's properties 
-to that new blog doc (database) for us
-
-*/
-
-},{ timestapms: true})
-
-
-
-/*
--The model wraps the scheama and provides a mneas to 
-communicate with the data base collection
-
--First argument is the name of the model
-->looks at blogs collection in MongoDB
-
--Pass in ths schame you created on line 18
-*/
-
-
-
-const Blog = mongoose.model('Blog'. blogSchema);
-
-//Exporting module so we can use it in our code;
+const Blog = mongoose.model('Blog', blogSchema);
 module.exports = Blog;
-
