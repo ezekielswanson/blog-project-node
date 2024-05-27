786b033d4f1131c52769a53ffde0897896ddabbe
ezekielswanson <swansonezekiel@gmail.com>
2024-05-27 18:29:53 -0500

    Updated files

---
+++ controllers/blogController.js
@@ -0,0 +1,29 @@
+/* What file am I working with the model on? 
+Import that file here 
+*/
+
+/* Aside
+
+//importing blog model allowing you to put routes on the 
+//blog model
+//.. means go out of the current directory into another
+//code runs from top to bottom
+
+*/
+
+const Blog = require('../models/blog');
+const blogController = require('../controller/blogController');
+
+const blog_index = (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then(result => {
+            res.render('index', { blogs: result, title: 'All blogs' });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+module.exports = {
+    blog_index
+};
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
 const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
@@ -94,97 +94,11 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+//scroping with url
+//DRY - allows you to change the URL structure in 1 place
+app.use('/blogs', blogRoutes)
 
 
-/*
-//Adding blog to db
-app.post('/blogs', (req, res) => {
-    const blog = new Blog({
-      title: 'New blog added',
-      snippet: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
-      body: 'The full content of the blog.'
-    });
-  
-    blog.save()
-      .then(result => {
-        res.redirect('/blogs');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
-*/
-
-//post
-app.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-})
-  
-
-//handling blog id
-// : in front of id denotes it's an id
-app.get('/blogs/:id', (req, res) =>{
-  //search in DB for doc w/ id
-
-  //get id
-  const id = req.params.id;
-  console.log(id);
-
-  //get document from db with that id
-  /*
-  -Blog is the model in the database
-  -method on that model and the method 
-  takes in data (id)
-  -"think pass in data into the method/function"
-  */
-
-  //find by id then render data
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', {blog: result, title: "Blog Details"})
-    })
-    .catch(err => {
-      console.log("id incorrect", err)
-
-    })
-
-})
-
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  
-  //in node can't send redirect as response when sending ajax req
-  /*
-  -will send json data BACK to brwoser and that json data has redirect prop
-  -when data's recieved back in details.ejs look @ redirect prop 
-  and this will be where we redirect. Based on the redirect prop  in jSON
-  */
-
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-
-    })
-})
 
 // 404 page
 app.use((req, res) => {
