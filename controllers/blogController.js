085da5f35c5d8fa2c181d71a0fb2cbc6fac17680
ezekielswanson <swansonezekiel@gmail.com>
2024-05-27 19:14:26 -0500

    Updated blog controller css and blogRoutes files

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -15,14 +15,73 @@ const Blog = require('../models/blog');
 
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { blogs: result, title: 'All blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
 
+
+
+const blog_details = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
+ //in node can't send redirect as response when sending ajax req
+    /*
+    -will send json data BACK to brwoser and that json data has redirect prop
+    -when data's recieved back in details.ejs look @ redirect prop 
+    and this will be where we redirect. Based on the redirect prop  in jSON
+    */
+
+
+    const blog_create_get = (req, res) => {
+        res.render('create', { title: 'Create a new blog' });
+      }
+      
+      const blog_create_post = (req, res) => {
+        const blog = new Blog(req.body);
+        blog.save()
+          .then(result => {
+            res.redirect('/blogs');
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+      
+      const blog_delete = (req, res) => {
+        const id = req.params.id;
+        Blog.findByIdAndDelete(id)
+          .then(result => {
+            res.json({ redirect: '/blogs' });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+      
+
+
+
+
+//This makes blog_index available as a property of the exported object.
 module.exports = {
-    blog_index
+    blog_index, 
+    blog_details, 
+    blog_create_get, 
+    blog_create_post, 
+    blog_delete
 };
+
+
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -9,93 +9,15 @@ const blogController = require('../controller/blogController');
 const router = express.Router();
 
 
-router.get('/', blogController.blog_index)
-
-router.get('/', (req, res) => {
-   
-  });
-  
-  
-  /*
-  //Adding blog to dbg
-  router.post('/blogs', (req, res) => {
-      const blog = new Blog({
-        title: 'New blog added',
-        snippet: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
-        body: 'The full content of the blog.'
-      });
-    
-      blog.save()
-        .then(result => {
-          res.redirect('/blogs');
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-  
-  */
-  
-  //post
-  router.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-  
-    blog.save()
-      .then((result) => {
-        res.redirect('/blogs');
-      })
-  })
-    
-  
-  //handling blog id
-  // : in front of id denotes it's an id
-  router.get('/blogs/:id', (req, res) =>{
-    //search in DB for doc w/ id
-  
-    //get id
-    const id = req.params.id;
-    console.log(id);
-  
-    //get document from db with that id
-    /*
-    -Blog is the model in the database
-    -method on that model and the method 
-    takes in data (id)
-    -"think pass in data into the method/function"
-    */
-  
-    //find by id then render data
-    Blog.findById(id)
-      .then(result => {
-        res.render('details', {blog: result, title: "Blog Details"})
-      })
-      .catch(err => {
-        console.log("id incorrect", err)
-  
-      })
-  
-  })
-  
-  
-  router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    //in node can't send redirect as response when sending ajax req
-    /*
-    -will send json data BACK to brwoser and that json data has redirect prop
-    -when data's recieved back in details.ejs look @ redirect prop 
-    and this will be where we redirect. Based on the redirect prop  in jSON
-    */
-  
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: '/blogs' })
-      })
-      .catch(err => {
-        console.log("id incorrect", err)
-  
-      })
-  })
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 
-  module.exports = router;
+
+
+
+
+module.exports = router;
